895df208fe4094081b5b0cb4392b44eca8f54dab
Public Meme Token <145687065+PublicMemeToken@users.noreply.github.com>
2023-10-14 13:26:07 +0200

    Add files via upload

---
+++ pages/stake.tsx
@@ -0,0 +1,58 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/stake.module.css";
+
+const stake1: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>Choose Your NFT Type! </h1>
+      <div className={styles.nftBoxGrid}>
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake721`)}
+        >
+         
+          
+          <h2 className={styles.selectBoxTitle}>  "???? x PMT" NFT</h2>
+          <p className={styles.selectBoxDescription}>
+             <b>35% APY</b>
+          </p>
+          <p className={styles.selectBoxDescription1}>
+             <b>ERC721</b>
+          </p>
+        </div>
+
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake1155`)}
+        >
+          
+          
+          <h2 className={styles.selectBoxTitle}>Loylty NFT</h2>
+          <p className={styles.selectBoxDescription}>
+            <b>45% APY</b>
+          </p>
+          <p className={styles.selectBoxDescription1}>
+             <b>ERC1155</b>
+          </p>
+        </div>
+      </div>
+      <Image style={{ marginTop:150 }}
+              src="/logo.png"
+              width={320}
+              height={320}
+              alt="Home"
+              quality={100}
+              
+            />
+    </div>
+  );
+};
+
+export default stake1;
---
+++ pages/stake721.tsx
@@ -0,0 +1,183 @@
+import {
+  ConnectWallet,
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  useContractRead,
+  useContractWrite,
+  useOwnedNFTs,
+  useTokenBalance,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { BigNumber, ethers } from "ethers";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import NFTCard from "../components/NFTCard1";
+import {
+  nftDropContractAddress,
+  stakingContractAddress,
+  tokenContractAddress,
+} from "../const/contractAddresses1";
+import styles from "../styles/stakeNFT.module.css";
+import Image from "next/image";
+
+
+
+const Stake: NextPage = () => {
+  const address = useAddress();
+  const { contract: nftDropContract } = useContract(
+    nftDropContractAddress,
+    "nft-drop"
+  );
+  const { contract: tokenContract } = useContract(
+    tokenContractAddress,
+    "token"
+  );
+  const { contract, isLoading } = useContract(stakingContractAddress);
+  const { data: ownedNfts } = useOwnedNFTs(nftDropContract, address);
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+  const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
+  const { data: stakedTokens } = useContractRead(contract, "getStakeInfo", [
+    address,
+  ]);
+
+  useEffect(() => {
+    if (!contract || !address) return;
+
+    async function loadClaimableRewards() {
+      const stakeInfo = await contract?.call("getStakeInfoForToken", [
+        0,
+        address,
+      ]);
+      setClaimableRewards(stakeInfo[1]);
+    }
+
+    loadClaimableRewards();
+  }, [address, contract]);
+
+  async function stakeNft(id: string) {
+    if (!address) return;
+
+    const isApproved = await nftDropContract?.isApproved(
+      address,
+      stakingContractAddress
+    );
+    if (!isApproved) {
+      await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
+    }
+    await contract?.call("stake", [id, 1]);
+  }
+
+  if (isLoading) {
+    return <div className={styles.container}>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Stake your "???? x PMT" NFT</h1>
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+
+      {!address ? (
+        <ConnectWallet
+        theme={"dark"}
+        modalTitle={"PMT WALLET"}
+        modalSize={"wide"}
+        welcomeScreen={{
+          title: "Public Meme Token | $PMT",
+          img: {
+            src: "https://i.ibb.co/cbxC2h7/PMT-Frog-Dog.png",
+            width: 150,
+            height: 150,
+          },
+        }}
+        modalTitleIconUrl={
+          "https://i.ibb.co/5vy5cXF/favicon.png"
+        }
+      />
+      ) : (
+        <>
+          <h2>Your Tokens</h2>
+          <div className={styles.tokenGrid}>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
+              <p className={styles.tokenValue}>
+                <b>
+                  {!claimableRewards
+                    ? "No rewards"
+                    : ethers.utils.formatUnits(claimableRewards, 18)}
+                </b>{" "}
+                {tokenBalance?.symbol}
+              </p>
+            </div>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Current Balance</h3>
+              <p className={styles.tokenValue}>
+                <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
+              </p>
+            </div>
+          </div>
+
+          <Web3Button
+            action={(contract) => contract.call("claimRewards", [0])}
+            contractAddress={stakingContractAddress}
+          >
+            Claim Rewards
+          </Web3Button>
+          
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Staked NFTs</h2>
+          
+          <div className={styles.nftBoxGrid1}>
+            {stakedTokens &&
+              stakedTokens[0]?.map((stakedToken: BigNumber) => (
+                <NFTCard
+                  tokenId={stakedToken.toNumber()}
+                  key={stakedToken.toString()}
+                />
+                
+              ))}
+          </div>
+
+          <hr  className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Unstaked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {ownedNfts?.map((nft) => (
+              <div className={styles.nftBox} key={nft.metadata.id.toString()}>
+                <ThirdwebNftMedia
+                style={{ marginLeft:0 }}
+                  metadata={nft.metadata}
+                  className={styles.nftMedia}
+                />
+                <h3 style={{ marginLeft:0 }}>{nft.metadata.name}</h3>
+                <Web3Button
+                  contractAddress={stakingContractAddress}
+                  action={() => stakeNft(nft.metadata.id)}
+                  style={{ marginLeft:0 }}
+                >
+                  Stake
+                </Web3Button>
+                
+              </div>
+              
+            ))}
+          </div>
+          
+        </>
+      )}
+      <div >
+            <Image style={{ marginTop:150 }}
+              src="/logo.png"
+              width={320}
+              height={320}
+              alt="Home"
+              quality={100}
+              
+            />
+          </div>
+    </div>
+    
+  );
+};
+
+export default Stake;
