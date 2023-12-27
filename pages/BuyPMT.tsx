187803b276e304b4b89781199a542fde514f29f7
Public Meme Token <145687065+PublicMemeToken@users.noreply.github.com>
2023-12-27 15:55:49 +0100

    Update BuyPMT.tsx

--- pages/BuyPMT.tsx
+++ pages/BuyPMT.tsx
@@ -272,12 +272,12 @@ import {
             </p>
             
             <p className={styles.explain}>
-            <span >Sold </span> 0%
+            <span >Sold </span> 3%
               
             </p>
            
 
-            <Line  percent={0} strokeWidth={1} strokeColor="green" className={styles.blink_me}  trailColor="grey" trailWidth={1} />
+            <Line  percent={3} strokeWidth={1} strokeColor="green" className={styles.blink_me}  trailColor="grey" trailWidth={1} />
             
           </>
           
