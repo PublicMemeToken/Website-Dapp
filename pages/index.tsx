fa0c4632d4f31f327687bf13151e8f910c70bc14
Public Meme Token <145687065+PublicMemeToken@users.noreply.github.com>
2024-01-10 12:58:00 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,8 +115,8 @@ const Home: NextPage = () => {
           </div>
           <br></br>
           <br></br>
-          <h1 style={{ color:"#fbbe7c"  }} >PHASE 9 ENDS IN</h1>
-           <Countdown date={`${year}-01-10T13:00:00`} />
+          <h1 style={{ color:"#fbbe7c"  }} >PHASE 10 ENDS IN</h1>
+           <Countdown date={`${year}-01-17T13:00:00`} />
                    
               
               <br></br>
