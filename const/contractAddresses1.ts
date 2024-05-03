39ee795c7074734a32a5f2546a6bcee68a084314
Public Meme Token <145687065+PublicMemeToken@users.noreply.github.com>
2024-05-03 08:15:47 +0200

    Update contractAddresses1.ts

--- const/contractAddresses1.ts
+++ const/contractAddresses1.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
-  "0x6cBfc052a1454709dA7A5ECE75FB488c2D24cc39";
+  "0x2FbC96Bf8f5aC2eBa52cF992C654c4aAcFC2BaDd";
 export const tokenContractAddress =
   "0x68Ae2F202799be2008c89e2100257e66F77DA1f3";
 export const stakingContractAddress =
-  "0x4FF49cF729f5F2A8169F33Eec1751924C89bDa19";
+  "0x699feAfF0D8dd87320DAa8e3a7a6c4ba6DCe2b8C";
